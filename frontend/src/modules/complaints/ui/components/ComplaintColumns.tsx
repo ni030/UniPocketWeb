9e17a6dfd0d5b185cfd74bc14122af5b9fcba5ab
LIM SI NI <limsini000@gmail.com>
2025-03-17 23:56:24 +0800

    redeploy version

--- frontend/src/modules/complaints/ui/components/ComplaintColumns.tsx
+++ frontend/src/modules/complaints/ui/components/ComplaintColumns.tsx
@@ -55,7 +55,7 @@ export const columns: ColumnDef<Complaint>[] = [
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Complaint Id
+          Complaint ID
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
